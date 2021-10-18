be3dc0c1b14c7bd31cf4d9fa8afa1f0ef6444873
Judah-Irani <myfamily4jesus@gmail.com>
2021-10-18 17:35:23 +0530

    Added events trigger

---
+++ js/events.js
@@ -0,0 +1,43 @@
+const expectedOrigin = "https://livearea-pilot-approuter-caas2-sap.cfapps.us10.hana.ondemand.com/";
+
+const events = [];
+
+window.addEventListener(
+    "message",
+    event => {
+        if(event.origin === expectedOrigin) {
+            events.push(event.data);
+            document.getElementById("eventPrintout").innerHTML = JSON.stringify(events, null, 4);
+        }
+    },
+    false
+)
+
+sendStartupEvents();
+
+// required startup events to notify the consumer app of readiness and preferred iframe size
+function sendStartupEvents() {
+    let initEvent = { type: 'initialized' , data: null};
+    this.sendMessage(initEvent);
+
+    let sizeEvent = { type: 'sizeChange', data: { height: 400}}
+    this.sendMessage(sizeEvent);
+}
+
+function sendMessage(event) {
+    // web
+    if (window.parent !== window) {
+        window.parent.postMessage(event, expectedOrigin);
+    }
+    // android
+    else if (((window).Android)) {
+        ((window).Android).sendMessage(JSON.stringify(event));
+    }
+    // ios
+    else if ((window).webkit && (window).webkit.messageHandlers && (window).webkit.messageHandlers.upscaleHandler) {
+        (window).webkit.messageHandlers.upscaleHandler.postMessage(JSON.stringify(event));
+    }
+    else {
+        console.log('no send method detected');
+    }
+}
